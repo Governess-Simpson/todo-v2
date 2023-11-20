512517021270b87f67d12cf92ad630b3d5a8eb7c
Governess Simpson <GovernessS515@gmail.com>
2023-11-19 16:12:22 -0800

    remove unused code, removed debugging comments

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,11 +1,8 @@
 const updateButtons = document.querySelectorAll(".update");
-// const oldTexts = document.querySelectorAll(".text");
 let oldText;
 let form;
-// const forms = document.querySelectorAll(".reveal")
 let task_id;
 
-console.log("testing")
 function getId(button_id){
     task_id = button_id.replace('update-', '')
 }
@@ -15,8 +12,6 @@ updateButtons.forEach(btn => {
         e.preventDefault();
         oldText = document.getElementById(`text-${task_id}`);
         form = document.getElementById(`form-${task_id}`);
-        console.log(oldText, form, btn, `text-${task_id}`)
-
         form.style.display == 'none' ? form.style.display = 'block' : form.style.display = 'none';
         !oldText.style.display || oldText.style.display == 'block' ? oldText.style.display = 'none' : oldText.style.display = 'block';
         
